45ec7e614963eadcc89a23e26ff760a562c9be1b
Michal Altair Valášek <michal.valasek@altairis.cz>
2021-03-01 02:53:16 +0100

    Initial commit

---
+++ build.cmd
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+REM -- Delete output folder
+RMDIR /Q /S docs
+
+REM -- Mirror the site
+START CMD /C "dotnet run --project src"
+tools\wget -P docs -nH --mirror http://localhost:5000/
+
+REM -- Fix files
+REN docs\* *.html
+REN docs\prohlaseni\* *.html
+RMDIR /Q /S docs\Content
+XCOPY /S src\wwwroot\Content docs\Content\
