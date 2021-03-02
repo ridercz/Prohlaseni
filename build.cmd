356cd00d63934cc76148a08d9458f1c4137b2e37
Michal Altair Valášek <michal.valasek@altairis.cz>
2021-03-02 14:34:49 +0100

    Add favicon.

--- build.cmd
+++ build.cmd
@@ -10,6 +10,7 @@ tools\wget -T 0 --retry-connrefused -P docs -nH --mirror http://localhost:5000/
 REM -- Fix files
 REN docs\* *.html
 REN docs\prohlaseni\* *.html
+REN docs\favicon.html favicon.ico
 RMDIR /Q /S docs\Content
 XCOPY /S src\wwwroot\Content docs\Content\
 
